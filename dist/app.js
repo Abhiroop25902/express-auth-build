aa42121ab23d319ed29b3d428420f3a4e6545c7e
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-11-14 00:00:26 +0530

    remove package-lock.json from gitignore

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const healthRouter_1 = __importDefault(require("./health/healthRouter"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+app.use('/health', healthRouter_1.default);
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
